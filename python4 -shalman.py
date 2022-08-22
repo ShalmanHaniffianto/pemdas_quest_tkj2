16673158811c37ddeeeec31c47ad3c3390819bcb
Shalman <111479754+ShalmanHaniffianto@users.noreply.github.com>
2022-08-21 23:23:45 -0700

    Add files via upload

---
+++ python4 -shalman.py
@@ -0,0 +1,30 @@
+print("===================")
+print("Rumus Bangun Datar")
+print("====================\n")
+
+print("1. persegi\n")
+
+sisi = int(input("masukan nilai sisi: "))
+luas_p = sisi * sisi
+print("luas persegi adalah",luas_p)
+
+print("2.persegi panjang\n")
+
+panjang = int(input("Masukan nilai panjang: "))
+lebar = int(input("masukan nilai lebar: "))
+luas_pp = panjang * lebar
+
+print("luas persegi panjang adalah: ",luas_pp)
+
+print("3.segitiga\n")
+
+alas = int(input("masukan nilai alas: "))
+tinggi = int(input("masukan nilai tinggi: "))
+
+luas_s = alas * tinggi / 2
+
+print("luas segitiga adalah: ",luas_s)
+
+
+
+
