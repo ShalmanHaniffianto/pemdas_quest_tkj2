f9acf11e3a93a279bac1eb975e93d57934344f44
Shalman <111479754+ShalmanHaniffianto@users.noreply.github.com>
2022-08-21 21:55:02 -0700

    Add files via upload

---
+++ python-latihan.py
@@ -0,0 +1,16 @@
+S = input ("Nama depan anda: ")
+H = input("Nama belakang anda: ")
+K = input("Kelas anda: ")
+print("Nama anda adalah",S,H,"Kelas anda",K)
+
+a =int(input("masukan nilai a: "))
+b =float(input("masukan nilai b: "))
+c= a+b
+
+print("A=", a)
+print("B=", b)
+print(a,"+",b,"=",c)
+
+x = int(input("masukan nilai mata uang: "))
+y = x * 13752
+print(x,"dollar =",y,"rupiah")
---
+++ python3-shalman.py
@@ -0,0 +1,15 @@
+d = input("masukan nama depan: ")
+c = input("masukan nama belakang: ")
+print("nama lengkap anda adalah",d,c)
+
+a = int(input("masukan nilai A: "))
+b = float(input("masukan nilai b: "))
+c = a + b
+
+print("A=" , a)
+print("B=" , b)
+print(a,"+",b,"=" ,c)
+
+x = int(input("masukan suhu celcius"))
+y = (9/5 * x) + 32
+print( x,"celcius =", y,"fahrenheit")
