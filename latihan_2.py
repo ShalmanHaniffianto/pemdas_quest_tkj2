b526e442ddc22b60302bfc85a47b444ece86100b
Shalman <111479754+ShalmanHaniffianto@users.noreply.github.com>
2023-02-13 09:31:42 +0700

    Add files via upload

---
+++ latihan_2.py
@@ -0,0 +1,12 @@
+def segitiga(alas,tinggi):
+    luas =alas * tinggi / 2
+    print("luas segitiga adalah:%i cm"%luas)
+    
+
+segitiga(20,30)
+
+def persegi_panjang(panjang,lebar):
+    luas = panjang * lebar
+    print("luas persegi panjang adalah:%icm"%luas)
+
+persegi_panjang(5,10)
---
+++ latihan_fungsi.py
@@ -0,0 +1,23 @@
+def latihan():
+    print("example")
+latihan()
+latihan()
+latihan() 
+
+'''fungsi dengan parameter'''
+'''def nama_fungsi(p1,p2)'''
+
+def say_hi(nama):
+    print("Halo perkenalkan nama saya",nama)
+
+say_hi("jawir")
+say_hi("filian")
+
+'''Buatlah fungsi dengan 3 parameter'''
+def bjirr(nama,agensi,generasi):
+    print("Halo saya",nama,)
+    print("Dari",agensi)
+    print("generasi ke",generasi)
+bjirr("Pomu","Nijisanji EN",1)
+
+
